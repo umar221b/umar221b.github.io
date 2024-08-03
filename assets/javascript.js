06402de05ef8961dd8a8d4151c3ced27f8cfd5e8
Umar Alkafaween <omar.ka923@gmail.com>
2024-08-03 04:59:39 +0300

    Load masonry after all images are loaded

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,7 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Load bootstrap tooltips
     const tooltipTriggerList = document.querySelectorAll('.more-info-tooltip');
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl, {
         container: '.about',
         placement: 'bottom',
     }));
+
+    Promise.all(Array.from(document.images).filter(img => !img.complete).map(img => new Promise(resolve => { img.onload = img.onerror = resolve; }))).then(() => {
+        // Load masonry galleries
+        const galleries = document.querySelectorAll('.gallery');
+        const initializedGalleries = [...galleries].map(gallery => new Masonry(gallery, {
+            percentPosition: true
+        }));
+    });
 }, false);
